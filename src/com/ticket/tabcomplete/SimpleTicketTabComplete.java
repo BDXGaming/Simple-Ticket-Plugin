f6d73ee0859095c9f46e1bbdab3bca7773916b43
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2021-05-30 12:07:19 -0400

    Add basic Tabcomplete to /ticket command

--- src/com/ticket/tabcomplete/SimpleTicketTabComplete.java
+++ src/com/ticket/tabcomplete/SimpleTicketTabComplete.java
@@ -1,13 +1,12 @@
 package com.ticket.tabcomplete;
 
-import com.ticket.commands.SimpleTicketsTicket;
 import com.ticket.commands.Ticket;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
-
-import java.lang.reflect.Array;
+import org.bukkit.entity.Player;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SimpleTicketTabComplete implements TabCompleter {
@@ -18,33 +17,35 @@ public class SimpleTicketTabComplete implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args){
         if(cmd.getName().equalsIgnoreCase("ticket")){
-            if (args.length == 1){
-                ArrayList<String> tab = new ArrayList<>();
-                if(!args[0].equals("")){
-                    for(String scmd: subcommands){
-                        if(scmd.startsWith(args[0].toLowerCase())){
-                            tab.add(scmd);
+            ArrayList<String> tab = new ArrayList<>();
+            if(sender instanceof Player){
+                if(sender.hasPermission("ticket.ticket.staff")) {
+                    if (args.length == 1) {
+                        if (!args[0].equals("")) {
+                            for (String scmd : subcommands) {
+                                if (scmd.startsWith(args[0].toLowerCase())) {
+                                    tab.add(scmd);
+                                }
+                            }
+                        } else {
+                            tab.addAll(Arrays.asList(subcommands));
                         }
+                        return tab;
+                    }
+
+                    if (args.length == 2) {
+
+                        if ((args[0].equalsIgnoreCase("claim"))|| args[0].equalsIgnoreCase("close") || args[0].equalsIgnoreCase("history") || args[0].equalsIgnoreCase("hist")){
+                            for (Ticket ticket : Ticket.getTickets()) {
+                                tab.add(ticket.getNum().toString());
+                            }
+                        }
+                        return tab;
                     }
                 }
-                else{
-                    for(String scmd: subcommands){
-                        tab.add(scmd);
-                    }
-                }
-                return tab;
             }
 
-            else if(args.length == 2){
-                ArrayList<String> tab = new ArrayList<>();
-                if(!args[1].equals("")){
-                    for (Ticket ticket: Ticket.getTickets()){
-                        tab.add(ticket.toString());
-                    }
-                }
-                return tab;
-            }
-
+            return tab;
         }
         return null;
     }
