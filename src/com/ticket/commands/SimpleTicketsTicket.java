42d332d60bbd58e2ba95aeecbd695fbeac8324f0
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2021-05-30 19:02:15 -0400

    Add needed return statements

--- src/com/ticket/commands/SimpleTicketsTicket.java
+++ src/com/ticket/commands/SimpleTicketsTicket.java
@@ -35,6 +35,11 @@ public class SimpleTicketsTicket implements CommandExecutor {
             if(player.hasPermission("ticket.ticket.staff")){
                 String message = Ticket.getAllTickets();
                 player.sendMessage(message);
+                return true;
+            }
+            else{
+                player.sendMessage(ChatColor.RED + "You do not have the permissions to use this command!");
+                return true;
             }
 
         }
@@ -42,83 +47,101 @@ public class SimpleTicketsTicket implements CommandExecutor {
         if(cmd.getName().equalsIgnoreCase("ticket")){
             if(player.hasPermission("ticket.ticket")){
 
-                if(player.hasPermission("ticket.ticket.staff")){
+                if(player.hasPermission("ticket.ticket.staff")) {
 
-                    if(args[0].equalsIgnoreCase("claim")){
-                        ArrayList<Player> staff_tickets = Ticket.getStaffWithTickets();
-                        if (!staff_tickets.contains(player)){
-                            Ticket t = Ticket.getTicket(args[1]);
-                            assert t != null;
-                            if (!(t.isClaimed())) {
-                                t.claimTicket(player);
-                                player.sendMessage("§aClaimed Ticket-" + t.getNum());
+                    if (args.length > 0) {
+
+                        if (args[0].equalsIgnoreCase("claim")) {
+                            ArrayList<Player> staff_tickets = Ticket.getStaffWithTickets();
+                            if (!staff_tickets.contains(player)) {
+                                Ticket t = Ticket.getTicket(args[1]);
+                                assert t != null;
+                                if (!(t.isClaimed())) {
+                                    t.claimTicket(player);
+                                    player.sendMessage("§aClaimed Ticket-" + t.getNum());
+                                    return true;
+                                } else {
+                                    player.sendMessage("§cThis ticket is already claimed by: " + t.getStaffClaimer().getDisplayName());
+                                    return true;
+                                }
                             } else {
-                                player.sendMessage("§cThis ticket is already claimed by: " + t.getStaffClaimer().getDisplayName());
+                                player.sendMessage(ChatColor.YELLOW + "You cannot claim more than one ticket at a time!");
+                                return true;
                             }
                         }
-                        else{
-                            player.sendMessage(ChatColor.YELLOW + "You cannot claim more than one ticket at a time!");
-                        }
-                    }
 
-                    //Closes and deletes the ticket
-                    else if(args[0].equalsIgnoreCase("close")){
-                        Ticket t = Ticket.getTicket(args[1]);
-                        player.sendMessage("§c[Ticket-" + args[1] +"]§c§l Deleted");
-                        if (t != null) {
-                            t.deleteTicket();
-                        }
-                    }
-
-                    //To view all the previous messages sent in the ticket both by staff and the player
-                    else if((args[0].equalsIgnoreCase("history")) || (args[0].equalsIgnoreCase("hist"))){
-                        try {
+                        //Closes and deletes the ticket
+                        else if (args[0].equalsIgnoreCase("close")) {
                             Ticket t = Ticket.getTicket(args[1]);
-                            player.sendMessage(t.getMsgLog());
-                        }catch (NullPointerException e){
-                            player.sendMessage(ChatColor.RED + "There are no open tickets at this time!");
+                            player.sendMessage("§c[Ticket-" + args[1] + "]§c§l Deleted");
+                            if (t != null) {
+                                t.deleteTicket();
+                            }
+                            return true;
                         }
-                    }
 
-                    else if(Ticket.hasClaim(player)){
-                        Ticket t = Ticket.getClaim(player);
-                        StringBuilder msg = new StringBuilder("§c[Ticket-" + t.getNum() + "]§a(" + player.getDisplayName() + "§a)§d ");
-                        for (int i = 0; i < args.length; i++) {
-                            msg.append(args[i]).append(" ");
-                        }
-                        Bukkit.getPlayer(t.getOwner().getUniqueId()).sendMessage(msg.toString());
-                        player.sendMessage(msg.toString());
-                        t.addmsg(msg.toString());
-                    }
-                }
-                else{
-                    if(Ticket.hasTicket(player)) {
-                        Ticket t = Ticket.getTicket(player);
-                        StringBuilder msg = new StringBuilder("§c[Ticket-" + t.getNum() + "]§a(" + player.getDisplayName() + "§a)§d ");
-                        for (int i = 0; i < args.length; i++) {
-                            msg.append(args[i]).append(" ");
-                        }
-                        if (!(t.isClaimed())) {
-                            for (Player s : staff) {
-                                s.sendMessage(msg.toString());
+                        //To view all the previous messages sent in the ticket both by staff and the player
+                        else if ((args[0].equalsIgnoreCase("history")) || (args[0].equalsIgnoreCase("hist"))) {
+                            try {
+                                Ticket t = Ticket.getTicket(args[1]);
+                                player.sendMessage(t.getMsgLog());
+                                return true;
+                            } catch (NullPointerException e) {
+                                player.sendMessage(ChatColor.RED + "There are no open tickets at this time!");
+                                return true;
+                            }
+                        } else if (Ticket.hasClaim(player)) {
+                            try {
+                                Ticket t = Ticket.getClaim(player);
+                                StringBuilder msg = new StringBuilder("§c[Ticket-" + t.getNum() + "]§a(" + player.getDisplayName() + "§a)§d ");
+                                for (int i = 0; i < args.length; i++) {
+                                    msg.append(args[i]).append(" ");
+                                }
+                                Bukkit.getPlayer(t.getOwner().getUniqueId()).sendMessage(msg.toString());
                                 player.sendMessage(msg.toString());
                                 t.addmsg(msg.toString());
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                return true;
                             }
-                        }
-                        else{
-                            Player staff = t.getStaffClaimer();
-                            staff.sendMessage(msg.toString());
-                            player.sendMessage(msg.toString());
-                            t.addmsg(msg.toString());
+                            return true;
+                        }else {
+                            try {
+                                if (Ticket.hasTicket(player)) {
+                                    Ticket t = Ticket.getTicket(player);
+                                    StringBuilder msg = new StringBuilder("§c[Ticket-" + t.getNum() + "]§a(" + player.getDisplayName() + "§a)§d ");
+                                    for (int i = 0; i < args.length; i++) {
+                                        msg.append(args[i]).append(" ");
+                                    }
+                                    if (!(t.isClaimed())) {
+                                        for (Player s : staff) {
+                                            s.sendMessage(msg.toString());
+                                            player.sendMessage(msg.toString());
+                                            t.addmsg(msg.toString());
+                                        }
+                                        return true;
+                                    } else {
+                                        Player staff = t.getStaffClaimer();
+                                        staff.sendMessage(msg.toString());
+                                        player.sendMessage(msg.toString());
+                                        t.addmsg(msg.toString());
+                                        return true;
+                                    }
+                                } else {
+                                    player.sendMessage("§cYou do not have an open ticket!");
+                                    return true;
+                                }
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                return false;
+                            }
                         }
                     }
                     else{
-                        player.sendMessage("§cYou do not have an open ticket!");
+                        return false;
                     }
                 }
             }
         }
-        return true;
+        return false;
     }
 
 }
