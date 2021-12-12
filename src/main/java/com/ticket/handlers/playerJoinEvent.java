4b180f2974a7c1adf553d80ea4ad64e71452ee3e
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2021-12-12 12:47:14 -0500

    sync active punishments on player join for low player counts

--- src/main/java/com/ticket/handlers/playerJoinEvent.java
+++ src/main/java/com/ticket/handlers/playerJoinEvent.java
@@ -1,9 +1,13 @@
 package com.ticket.handlers;
 
+import com.ticket.SimpleTicket;
+import com.ticket.punishment.Punishment;
+import com.ticket.punishment.PunishmentDatabase;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class playerJoinEvent implements Listener {
 
@@ -12,7 +16,14 @@ public class playerJoinEvent implements Listener {
 
         //Checks if the player that joined is the only player
         if(Bukkit.getServer().getOnlinePlayers().toArray().length <=1){
-            //pass
+
+            //Creates a new runnable which will get and check all the active punishments
+            new BukkitRunnable(){
+                @Override
+                public void run() {
+                    Punishment.checkPunishedPlayers();
+                }
+            }.runTaskAsynchronously(SimpleTicket.simpleTicket);
         }
 
     }
